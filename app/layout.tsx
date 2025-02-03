57e86a5f8bc32ff0910846fe1ad81483790604d7
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2025-02-03 23:09:37 +0100

    feat: add github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { ThemeToggle } from '@/components/theme-toggle';
 import { Toaster } from '@/components/ui/toaster';
+import { Github } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Link from 'next/link';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -26,6 +28,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <div className="absolute top-4 left-4 z-10">
+            <Link
+              href="https://github.com/svenliebig/learn-marathi"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground h-10 w-10"
+            >
+              <Github className="h-5 w-5" />
+              <span className="sr-only">GitHub Profile</span>
+            </Link>
+          </div>
           <div className="absolute top-4 right-4 z-10">
             <ThemeToggle />
           </div>
