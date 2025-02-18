9f180c67d9578b4eab2a8751efb4aa68b8fba1ce
Sven Liebig <9104499+svenliebig@users.noreply.github.com>
2025-02-18 21:35:18 +0100

    refactor: exercise service class

--- lib/services/exercise/exercise-service.ts
+++ lib/services/exercise/exercise-service.ts
@@ -4,24 +4,48 @@ import { progressService } from '../progress/progress-service'
 import { Challenge, ResolvedUserProgressService } from '../progress/types'
 import { Exercise } from './types'
 
+/**
+ * The service that handles the creation of exercises.
+ */
 export class ExerciseService {
   private readonly progressService: ResolvedUserProgressService
 
+  /** The amount of letters in an exercise */
+  public static readonly EXERCISE_SIZE = 8
+  /** The amount of oldest challenges that are definitely included in the exercise */
+  public static readonly OFFSET = 2
+  /** The amount of flawless challenges that triggers new letters */
+  public static readonly SOFT_FLAWLESS_THRESHOLD = 5
+
+  /**
+   * @param progressService The progress service to use.
+   */
   constructor(progressService: ResolvedUserProgressService) {
     this.progressService = progressService
   }
 
+  /**
+   * Returns an exercise for a user.
+   *
+   * @param userId The user's ID.
+   * @param module The module to get the exercise for.
+   *
+   * @returns The exercise.
+   */
   public async getExercise(userId: string, module: ExerciseMode): Promise<Exercise> {
-    const userProgress = await this.progressService.getResolvedUserProgress(userId)
+    try {
+      const userProgress = await this.progressService.getResolvedUserProgress(userId)
 
-    const challenged = userProgress.challenges.filter(challenge => challenge.module === module)
+      const challenged = userProgress.challenges.filter(challenge => challenge.module === module)
+      const letters = this.getExerciseLetters(challenged)
 
-    const letters = this.getExerciseLetters(challenged, module)
-
-    return {
-      mode: module,
-      size: 8,
-      letters,
+      return {
+        mode: module,
+        size: ExerciseService.EXERCISE_SIZE,
+        letters,
+      }
+    } catch (e) {
+      throw new Error('Failed to get exercise', { cause: e })
     }
   }
 
@@ -38,59 +62,67 @@ export class ExerciseService {
    * @returns
    */
   private getExerciseLetters(
-    challenges: Array<Omit<Challenge, 'module' | 'mistakes' | 'id' | 'attempts'>>,
-    mode: ExerciseMode
+    challenges: Array<Omit<Challenge, 'module' | 'mistakes' | 'id' | 'attempts'>>
   ): Letter[] {
     if (challenges.length <= 14) {
-      return this.getRandomLetters(8, 1, mode)
+      return LetterService.getRandomLetters(ExerciseService.EXERCISE_SIZE, 1)
     }
 
     const sorted = challenges.toSorted(
       (a, b) => a.lastActivity.getTime() - b.lastActivity.getTime()
     )
 
-    const lastTwo = sorted.splice(0, OFFSET)
+    const lastTwo = sorted.splice(0, ExerciseService.OFFSET)
     const flawed = this.removeFlawless(sorted)
 
-    const nextLetters = [...flawed.slice(0, 8 - lastTwo.length), ...lastTwo].map(challenge =>
-      LetterService.getLetter(challenge.letter)
-    )
+    const exerciseLetters = [
+      ...flawed.slice(0, ExerciseService.EXERCISE_SIZE - lastTwo.length),
+      ...lastTwo,
+    ].map(challenge => LetterService.getLetter(challenge.letter))
 
-    if (nextLetters.length < 8) {
-      let letterGenerator = LetterService.getEasiestLetters({
-        exclude: challenges.map(challenge => challenge.letter),
-      })
-
-      while (nextLetters.length < 8) {
-        const next = letterGenerator.next()
-
-        if (nextLetters.some(nextLetter => nextLetter.marathi === next.value.marathi)) {
-          continue
-        }
-
-        nextLetters.push({
-          latin: next.value.latin,
-          marathi: next.value.marathi,
-          difficulty: next.value.difficulty,
-        })
-      }
+    if (exerciseLetters.length === ExerciseService.EXERCISE_SIZE) {
+      return exerciseLetters
     }
 
-    return shuffle(nextLetters)
-  }
+    const letterGenerator = LetterService.getEasiestLetters({
+      exclude: challenges.map(challenge => challenge.letter),
+    })
 
-  private getRandomLetters(amount: number, level: number, mode: ExerciseMode): Letter[] {
-    return LetterService.getRandomLetters(amount, level)
+    while (exerciseLetters.length < ExerciseService.EXERCISE_SIZE) {
+      const next = letterGenerator.next()
+
+      if (next.done) {
+        /**
+         * this should not happen becase it's handled by the {@link LetterService.getEasiestLetters}
+         * but we're not sure what's going on here
+         */
+        throw new Error('Not enough unique letters available')
+      }
+
+      if (exerciseLetters.some(nextLetter => nextLetter.marathi === next.value.marathi)) {
+        continue
+      }
+
+      exerciseLetters.push({
+        latin: next.value.latin,
+        marathi: next.value.marathi,
+        difficulty: next.value.difficulty,
+      })
+    }
+
+    return shuffle(exerciseLetters)
   }
 
   private removeFlawless(
     challenges: Array<Omit<Challenge, 'module' | 'mistakes' | 'id' | 'attempts'>>
   ) {
-    return challenges.filter(challenge => challenge.flawless < FLAWLESS_THRESHOLD)
+    return challenges.filter(
+      challenge => challenge.flawless < ExerciseService.SOFT_FLAWLESS_THRESHOLD
+    )
   }
 }
 
-function shuffle(array: any[]) {
+function shuffle<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1))
     ;[array[i], array[j]] = [array[j], array[i]]
@@ -99,8 +131,3 @@ function shuffle(array: any[]) {
 }
 
 export const exerciseService = new ExerciseService(progressService)
-
-/** The amount of oldest challenges that are definitely included in the exercise */
-const OFFSET = 2
-/** The amount of flawless challenges that triggers new letters */
-const FLAWLESS_THRESHOLD = 5
